598fb2128de8be8267acdfa0b42620c9927586ef
= <nurali.cherniyazov03@gmail.com>
2024-09-21 17:55:57 +0500

    Initial

---
+++ Lab_1_best/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld/>
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+}
+</style>
+
